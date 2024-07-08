5f43386d0f8577d900e96ae96d91eb24f82dfb8a
preetish <pbakliwal@adobe.com>
2024-07-08 12:53:22 +0530

    cards css

--- blocks/godrej-block/godrej-block.js
+++ blocks/godrej-block/godrej-block.js
@@ -3,4 +3,8 @@ export default async function decorate(block) {
   const innerChildDiv = firstChildDiv.querySelector('div');
   firstChildDiv.classList.add('godrej-container');
   innerChildDiv.classList.add('content');
+  const secondChildDiv = firstChildDiv.querySelector('div:second-child');
+  if(secondChildDiv){
+    secondChildDiv.classList.add("second-content-block")
+  }
 }
