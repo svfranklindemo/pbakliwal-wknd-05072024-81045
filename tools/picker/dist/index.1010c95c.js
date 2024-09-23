49404ac4f0211793b5830e9302554c089cfeda25
preetish <pbakliwal@adobe.com>
2024-09-23 15:19:50 +0530

    fixing dist

--- tools/picker/dist/index.1010c95c.js
+++ tools/picker/dist/index.1010c95c.js
@@ -3555,6 +3555,6 @@ items:structuredClone(n.items),categories:structuredClone(n.categories),block:e,
                     </ul>
                 </td>
             </tr>
-        </table>`,selection:"multiple",type:"folder"}},getCategories:yf,getItems:yp,configFile:" https://main--xsc-wknd-commerce--hlxsites.hlx.live/configs.json",defaultConfig:"prod"}),ym);//# sourceMappingURL=index.1010c95c.js.map
+        </table>`,selection:"multiple",type:"folder"}},getCategories:yf,getItems:yp,configFile:" https://main--pbakliwal-wknd-05072024-81045--svfranklindemo.hlx.live/configs.json",defaultConfig:"prod"}),ym);//# sourceMappingURL=index.1010c95c.js.map
 
 //# sourceMappingURL=index.1010c95c.js.map
